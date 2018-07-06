c67d2852b41d099dbd75004ea1b350e4af9e394d
Sopuru Obiese <sopuruobiesie@gmail.com>
2018-07-06 10:41:28 +0100

    tolkein"s Poem

--- soso.js
+++ soso.js
@@ -1,2 +1,9 @@
 all that is gold does not glitter
 not all who wander are lost
+theold that is strog does not wither
+deep roots are not reached by the frost
+
+from the ashes, a fire shall be woken
+a light fron the shadows shall spring
+renewed shall be the blade that was broken
+the crownless again shall be king
